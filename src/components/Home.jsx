66b312d353c5b62e15e81befeeef7c7d2a87a06b
somesh-awasthi <156050321+somesh-awasthi@users.noreply.github.com>
2024-11-29 15:50:26 +0530

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Body from './Body';
 import Navbar from './Navbar';
 import '../style/Home.css';
